78c55de9fc7e228e7a02d92536407e0befad2082
Rosaverde <freez@interia.eu>
2019-11-09 14:32:33 +0000

    Update 2

--- reservation.php
+++ reservation.php
@@ -1,128 +1,148 @@
 <?php
-    session_start();
-	    if((!isset($_SESSION['logIn'])) && ($_SESSION['logIn']==false))
-        {
-            header('Location: LOGIN');
-            exit();
-        }
+session_start();
 ?>
+
 <!DOCTYPE html>
 <html>
 
-<head>    
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Purrrfect Hotel</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-        crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <body>
-  <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="http://bigittech.com/pethotel/">Purrrfect Hotel</a>
+    <nav class="navbar navbar-expand-lg navbar-dark px-5">
+        <a class="navbar-brand px-3" href="http://bigittech.com/pethotel">Purrrfect Hotel</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav  mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link px-3" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="contact.html">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="pricing.html">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="gallery.html">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="reservation.html">Reservation</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav px-5">
+                <li class="nav-item">
+                    <a class="nav-link" href="SignUp.html"><i class="fas fa-user-plus"></i>SignUp</a>
+                </li>
+                <?php
+                if ((isset($_SESSION['logIn'])) && ($_SESSION['logIn'] == true)) 
+                    {
+                        echo '<li class="nav-item dropdown">';
+                        echo '<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">';
+                    }
+                else
+                    {
+                        echo '<li class="nav-item">';
+                        echo '<a class="nav-link" href="LOGIN">';
+                    }
+                ?>
+                <i class="fas fa-user"></i>
+                <?php
+                if ((isset($_SESSION['logIn'])) && ($_SESSION['logIn'] == true)) 
+                    {
+                        echo 'My Account</a>';
+                        echo '<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">';
+                        echo '<a class="dropdown-item" href="profile">Profile</a>';
+                        echo '<a class="dropdown-item" href="reservation">Reservations</a>';
+                        echo '<a class="dropdown-item" href="logout_handler.php">Log Out</a>';
+                        echo '</div></li>';
+                    } 
+                else 
+                    {
+                        echo 'Log In</a></li>';
+                    }
+                ?>
+            </ul>
+        </div>
+    </nav>
+
+
+    <div class="mainContainer">
+        <h1><i class="fas fa-paw"></i> RESERVATION</h1>
+        <div class="row">
+            <div class="col-lg-12">
+                <form method="post">
+                    <div class="tbox">
+                            <select class="services" name="option">
+                                    <option value="">Standard Room $45</option>
+                                    <option value="">Vip Room $65</option>
+                                    <option value="">Daycare FULL DAY $35</option>
+                                    <option value="">Daycare HALF DAY $25</option>
+                                </select>
+                    </div>
+                
+                    <label for="startDate"> From
+                        <div class="tbox">
+                            <input type="date" value="" name="startDate">
+                        </div>
+                    </label>
+                    <label for="endDate"> To
+                        <div class="tbox">
+                            <input type="date" value="" name="endTime">
+                        </div>
+                    </label>
+                    <input class="btnSubmit btnHov" type="submit" value="SUBMIT">
+                </form>
             </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="http://bigittech.com/pethotel/">Home</a></li>
-                    <li><a href="about">About</a></li>
-                    <li><a href="contact">Contact</a></li>
-                    <li><a href="pricing">Pricing</a></li>
-                    <li><a href="gallery">Gallery</a></li>
-                    <li><a href="reservation">Reservation</a></li>
-
-                    <?php
-                    // Adding Forex nav button after logging
-                        if((isset($_SESSION['logIn'])) && ($_SESSION['logIn']==true))
-                            {
-                                echo '<li><a href="forex.php">Forex</a></li>';
-                            }
-                    ?>
-
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="SignUp.php"><i class="fas fa-user-plus"></i> Sign Up</a></li>
-                    <?php
-                        //Toggle login or log out file           
-                            if((isset($_SESSION['logIn'])) && ($_SESSION['logIn']==true))
-                                {
-                                    echo '<li class="noHover"><a>';
-                                }
-                            else {
-                                {
-                                    echo '<li><a href="LOGIN">';
-                                }
-                            }
-                        ?>
-                        <i class="fas fa-user"></i>
-                        <?php
-                        // Adding log out after loging
-                        if((isset($_SESSION['logIn'])) && ($_SESSION['logIn']==true))
-                            {
-                            echo 'Welcome   '.$_SESSION['user'];
-                            echo '<li class="btnLogOut"><a href="logout_handler">Log Out!</a></li>';
-                            
-                            }
-                            else
-                            {
-                                echo 'Login';
-                            }
-                        ?>
-                    </a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-  </nav>
-  <h1>FOREX STRATEGIES</h1>
-  <div id="for">
-    <a href="/forex/MovingAverageEA" class="btn btn-default btn-lg" ><i class="fas fa-chart-line"></i>MovingAverageEA</a>
-    <hr>
-    <a href="/forex/Sure-Fire" class="btn btn-default btn-lg" ><i class="fas fa-chart-line"></i>Sure-Fire EA</a>
-    <hr>
-    <a href="/forex/SarEA" class="btn btn-default btn-lg" ><i class="fas fa-chart-line"></i>SarEA</a>
-    <hr>
-    <a href="/forex/Momentum_Trade" class="btn btn-default btn-lg" ><i class="fas fa-chart-line"></i>5 Min Momentum Trade</a>
-    <hr>
-    <a href="/forex/PivotPoint" class="btn btn-default btn-lg" ><i class="fas fa-chart-line"></i>PivotPoint</a>
-    <hr>
-    <a href="/forex/NewsEA" class="btn btn-default btn-lg" ><i class="fas fa-chart-line"></i>News</a>
-    <hr>
-    <a href="/forex/FraktalEA" class="btn btn-default btn-lg" ><i class="fas fa-chart-line"></i>Fraktale</a>
-    <hr>
-    <a href="/forex/FaleEliota" class="btn btn-default btn-lg" ><i class="fas fa-chart-line"></i>FaleEliota  </a>
-    <hr>
-<!--<a href="" class="btn btn-default btn-lg" ><i class="fas fa-paw"></i></a>
-    <hr>
-    <a href="" class="btn btn-default btn-lg" ><i class="fas fa-paw"></i></a>
-    <hr>
-    <a href="https://www.mql5.com/en/articles/1572?utm_campaign=articles.list&utm_medium=special&utm_source=mt5editor " class="btn btn-default btn-lg" ><i class="fas fa-paw"></i>Protect Yourselves, Developers! </a>
-    <hr>
-    <a href="" class="btn btn-default btn-lg" ><i class="fas fa-paw"></i></a>
-    <hr>
-    <a href="https://admiralmarkets.com/education/articles/forex-strategy/simple-forex-scalping-strategies-and-techniques " class="btn btn-default btn-lg" ><i class="fas fa-paw"></i>Simple Forex Scalping Strategies And Techniques <h3>Admiral Markets</h3>  </a>
-    <hr>
-    <a href="https://admiralmarkets.com/education/articles/forex-strategy/forex-1-minute-scalping-strategy-explained " class="btn btn-default btn-lg" ><i class="fas fa-paw"></i>Forex 1-Minute Scalping Strategy Explained <h3>Admiral Markets</h3> </a>
-    <hr>
-    <a href="https://admiralmarkets.pl/education/articles/forex-indicators/kanal-keltnera " class="btn btn-default btn-lg" ><i class="fas fa-paw"></i>Kanał Keltnera na Platformie MetaTrader [Poradnik Tradera] </a>
-    <hr>
-    <a href="https://www.mql5.com/en/code/12872" class="btn btn-default btn-lg" ><i class="fas fa-paw"></i> Angry Bird (Scalping)</a>
-		<hr>
-    <a href="https://www.mql5.com/en/forum/43518/page14 " class="btn btn-default btn-lg" ><i class="fas fa-paw"></i>Experts: Angry Bird (Scalping) </a> 
-		<hr>-->
-    </div>	
+        </div>
+        
+        <div class="reservationDetails" >
+            <h2> Your Reservation</h2>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form method="post">
+                        <div class="tbox">
+                                <select class="services" name="option" disabled>
+                                        <option value="">Standard Room $45</option>
+                                        <option value="">Vip Room $65</option>
+                                        <option value="">Daycare FULL DAY $35</option>
+                                        <option value="">Daycare HALF DAY $25</option>
+                                    </select>
+                        </div>
+                        <label for="startDate"> From
+                            <div class="tbox">
+                                <input type="date" value="" name="startDate" disabled>
+                            </div>
+        
+                        </label>
+                        <label for="endDate"> To
+                            <div class="tbox">
+                                <input type="date" value="" name="endTime" disabled>
+                            </div>
+                        </label>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <footer class="footerWrap">
         <p>Copyright © 2019  </p>
     </footer>
+
 <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
--- welcome.php
+++ welcome.php
@@ -1,135 +1,108 @@
 <?php
-  session_start();
- if((!isset($_SESSION['registrationComplete'])))
-       {
-           header('Location:index.php');
-           exit();
-       }
-   else
-       {
-           unset($_SESSION['registrationComplete']);
-       }
+session_start();
+    if((!isset($_SESSION['registrationComplete'])))
+        {
+            header('Location:index.php');
+            exit();
+        }
+    else
+        {
+            unset($_SESSION['registrationComplete']);
+        }
 ?>
- <!DOCTYPE html>
+
+<!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Purrrfect Hotel</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Purrrfect Hotel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet"> 
-<link rel="stylesheet" type="text/css" href="main.css">
-
+    <link rel="stylesheet" type="text/css" href="main.css">
 </head>
+
 <body>
-  
-<nav class="navbar navbar-default">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="http://bigittech.com/pethotel/">Purrrfect Hotel</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="http://bigittech.com/pethotel/">Home</a></li>
-                    <li><a href="about">About</a></li>
-                    <li><a href="contact">Contact</a></li>
-                    <li><a href="pricing">Pricing</a></li>
-                    <li><a href="gallery">Gallery</a></li>
-                    <li><a href="reservation">Reservation</a></li>
-
-        <?php
-                    // Adding Forex nav button after logging
-                        if((isset($_SESSION['logIn'])) && ($_SESSION['logIn']==true))
-                            {
-                                echo '<li><a href="forex.php">Forex</a></li>';
-                            }
-                    ?>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-                    <li><a href="SignUp.php"><i class="fas fa-user-plus"></i> Sign Up</a></li>
-                    <?php
-                        //Toggle login or log out file           
-                            if((isset($_SESSION['logIn'])) && ($_SESSION['logIn']==true))
-                                {
-                                    echo '<li class="noHover"><a>';
-                                }
-                            else {
-                                {
-                                    echo '<li><a href="LOGIN">';
-                                }
-                            }
-                        ?>
-                        <i class="fas fa-user"></i>
-                        <?php
-                        // Adding log out after loging
-                        if((isset($_SESSION['logIn'])) && ($_SESSION['logIn']==true))
-                            {
-                            echo 'Welcome   '.$_SESSION['user'];
-                            echo '<li class="btnLogOut"><a href="logout_handler">Log Out!</a></li>';
-                            
-                            }
-                            else
-                            {
-                                echo 'Login';
-                            }
-                        ?>
-                    </a></li>
+    <nav class="navbar navbar-expand-lg navbar-dark px-5">
+        <a class="navbar-brand px-3" href="http://bigittech.com/pethotel">Purrrfect Hotel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav  mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link px-3" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="about.html">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="contact.html">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="pricing.html">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="gallery.html">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="reservation.html">Reservation</a>
+                    </li>
                 </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-<div id="content">
-    <div class="login">
-        <h1><i class="fas fa-paw"></i>  LOG IN</h1>
-        <div class="welcome">You are registered. <br>Now you can log in.
-        </div>
-        <form action="login_handler" method="post">
-            <div class="tbox">
-                <input type="text" placeholder="username" name="login">
+                <ul  class="navbar-nav px-5">
+                    <li class="nav-item">
+                        <a class="nav-link" href="SignUp.html"><i class="fas fa-user-plus"></i>SignUp</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="LOGIN.html"><i class="fas fa-user"></i>My Account</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                         My Account
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <a class="dropdown-item" href="profile.html">Profile</a>
+                          <a class="dropdown-item" href="reservation.html">Reservations</a>
+                          <a class="dropdown-item" href="logout_handler.php">Log Out</a>
+                        </div>
+                      </li>
+                </ul>
             </div>
-            <div class="tbox">
-                <input type="password" placeholder="password" name="haslo">
+    </nav>
+
+        <div class="mainContainer">
+                <h1 class=" mb-5"><i class="fas fa-paw"></i> LOG IN</h1>
+                <div class="welcome my-4">
+                    <div>
+                        You are registered.
+                </div> 
+                <div>
+                        Now you can log in.
+                </div>
+                </div>
+                <form action="login_handler.php" method="post">
+                    <div class="tbox">
+                        <input type="text" placeholder="username" name="login">
+                    </div>
+                    <div class="tbox">
+                        <input type="password" placeholder="password" name="password">
+                    </div>
+                    <input class="btnSubmit btnHov" type="submit" value="LOG IN">
+                </form>
+                <a class="b1 btnHov" href="#">FORGOT PASSWORD ?</a>
+                <a class="b2 btnHov" href="SignUp.php">CREATE AN ACCOUNT</a>
             </div>
-            <input class="btn_log" type="submit"  value="LOG IN">
-        </form>
-        <?php
-        if(isset($_SESSION['error'])) echo $_SESSION['error'];  /*Your username or password is incorrect. */
-          {
-           
-            unset($_SESSION['error']);
-          }
-        ?>
-        <a class="b1" href="#">FORGOT PASSWORD ?</a>
-        <a class="b2" href="#">CREATE AN ACCOUNT</a>
-       
-    </div>
-</div> 
 
-<!-- Latest compiled and minified JavaScript -->
-<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
-
-<!-- 
-Logowanie
-1 Dodanie kilku zwierzat
-2 Opis zwierzat
-3 Mozliwosc gry ze zwierzetami
-4 Memeory mozliwosc dodania kilku zwierzat przy wyliczeniu wielkosci pola
- * Przy trafieniu dzwiek "Purrfekt"
-
- 
- -->
+        <footer class="footerWrap">
+                <p>Copyright © 2019 </p>
+            </footer>
+        
+        <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        </body>
+        
+        </html>
